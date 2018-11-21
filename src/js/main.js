dec760cecc56e652a02cb1a5af543ff845f8faa8
emilymeng <eeng@seattletimes.com>
2018-11-21 00:08:26 -0800

    all races

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,18 @@ var ich = require("icanhaz");
 var templateFile = require("./_popup.html");
 ich.addTemplate("popup", templateFile);
 
-var data = require("./allDistrict.geo.json");
+var overallLegend = require("./_overallLegend.html");
+ich.addTemplate("overallLegend", overallLegend);
+
+var dataAll = require("./allDistrict.geo.json");
+var dataAsian = require("./asian.geo.json");
+var dataBlack = require("./black.geo.json");
+var dataLatino = require("./latino.geo.json");
+var dataMulti = require("./multi.geo.json");
+var dataNative = require("./native.geo.json");
+var dataPi = require("./pi.geo.json");
+var dataWhite = require("./white.geo.json");
+var data = dataAll;
 
 var commafy = s => (s * 1).toLocaleString().replace(/\.0+$/, "");
 
@@ -87,7 +98,7 @@ var style = function(feature) {
       fillColor: getColor(feature.properties),
       weight: 1,
       opacity: .5,
-      color: '#000',
+      color: '#003531',
       fillOpacity: 0.6
     };
     return s;
@@ -99,3 +110,49 @@ var style = function(feature) {
   }).addTo(map);
 
  map.scrollWheelZoom.disable();
+
+ document.querySelector(".key").innerHTML = ich.overallLegend();
+
+// var select = document.querySelector('click');
+// var para = document.querySelector('p');
+
+Array.prototype.slice.call(document.querySelectorAll('.tab')).forEach(function(tab) {
+  tab.addEventListener("click", function() {
+    if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
+    tab.classList.add("selected");
+    var race = tab.innerHTML;
+
+    if (race === 'allDistricts') {
+      data = dataAll;
+    } else if (race === 'Asian') {
+      data = dataAsian;
+    } else if (race === 'Black') {
+      data = dataBlack;
+    } else if (race === 'Latino') {
+      data = dataLatino;
+    } else if (race === 'Multiracial') {
+      data = dataMulti;
+    } else if (race === 'Native') {
+      data = dataNative;
+    } else if (race === 'Pacific Islander') {
+      data = dataPi;
+    } else if (race === 'White') {
+      data = dataWhite;
+    }
+
+    data.features.forEach(function(f) {
+      ["persoc", "pertoc"].forEach(function(prop) {
+        f.properties[prop] = (f.properties[prop] * 100).toFixed(1);
+      });
+      ["totalstudents", "soc", "totalteachers", "toc"].forEach(function(prop) {
+        f.properties[prop] = commafy ((f.properties[prop]));
+      });
+    });
+
+    map.removeLayer(geojson);
+    geojson = L.geoJson(data, {
+      style: style,
+      onEachFeature: onEachFeature
+    }).addTo(map);
+  })
+});
